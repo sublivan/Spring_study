16590c0648422b705b56fbe51e348186ace1845e
김민섭 <33247303+meeansub@users.noreply.github.com>
2021-01-12 01:32:10 +0900

    update

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -3,13 +3,27 @@ package hello.hellospring;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig { // 직접 스프링 빈 등록하기(의존성 주입하기)
 
-//    @Bean
+//    private DataSource dataSource;
+/*
+DataSource는 데이터베이스 커넥션을 획득할 때 사용하는 객체다. 스프링 부트는 데이터베이스 커넥션
+정보를 바탕으로 DataSource를 생성하고 스프링 빈으로 만들어둔다. 그래서 DI를 받을 수 있다
+ */
+
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+    //    @Bean
 //    public MemberService memberService() {
 //        return new MemberService(memberRepository());
 //    }

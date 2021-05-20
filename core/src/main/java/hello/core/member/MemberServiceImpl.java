7f55e63ca83c17aed0f1aecdd7aa8d0a0014d683
meeansub <welcltm@gmail.com>
2021-05-20 18:00:45 +0900

    new class update

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,24 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    // DIP 위반
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}

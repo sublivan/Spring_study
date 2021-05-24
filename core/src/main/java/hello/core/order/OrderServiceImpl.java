8a4d3d2c4c3bb4ea36c9a3a38e1234eb3f0ab0bc
KIM MIN SUB <welcltm@gmail.com>
2021-05-24 16:44:51 +0900

    다양한 의존관계 주입 방법

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -17,8 +17,25 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    //필드 주입
+    //필드 인젝션은 안쓰는게 좋다. 
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
+
+   // 수정자 주입(setter 주입)
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy)  {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+
     // 생성자 주입
-    @Autowired
+    @Autowired //생성자가 하나일때는 생략해도 @Component를 통해 의존성주입이된다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;

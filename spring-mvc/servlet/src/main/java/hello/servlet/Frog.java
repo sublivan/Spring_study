9535b0325f06f17a8f4cff6ce7f903ea366fc7a7
KIM MIN SUB <welcltm@gmail.com>
2021-06-11 17:53:35 +0900

    서블릿, jsp

---
+++ spring-mvc/servlet/src/main/java/hello/servlet/Frog.java
@@ -0,0 +1,19 @@
+package hello.servlet;
+
+import java.util.Random;
+
+/**
+ * 하루에 3마리의 파리를 먹어야 살 수 잇는 개구리가 있다
+ * 파리가 개구리 앞을 지날 때, 개구리가 파리를 잡을 확률은 50%이다.
+ * 5번째로 개구리 앞을 지나가는 파리가 있다면, 개구리에게 잡힐 확률은?
+ */
+public class Frog {
+
+    public static void main(String[] args) {
+        int frogCapacity = 3;
+        int flyNumber = 5;
+        double probability;
+
+    }
+
+}

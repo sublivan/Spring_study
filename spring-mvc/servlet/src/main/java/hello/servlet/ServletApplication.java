52b354b61e1ef09cf23677e62050c6358c8e5182
KIM MIN SUB <welcltm@gmail.com>
2021-06-16 17:52:03 +0900

    주석추가

--- spring-mvc/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ spring-mvc/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan
+@ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 

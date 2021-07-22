4d81ba66a77ffba89f12376b4d2f41557d90c554
meeansub <welcltm@gmail.com>
2021-07-22 17:50:05 +0900

    주석 추가

--- spring-mvc/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring-mvc/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -69,12 +69,21 @@ public class BasicItemController {
        return "basic/item";
     }
 
+    /**
+     * @ModelAttribute name 생략 가능
+     * model.addAttribute(item); 자동 추가, 생략 가능
+     * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+     */
 //    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item) {
         itemRepository.save(item);
         return "basic/item";
     }
 
+    /**
+     * @ModelAttribute 자체 생략 가능
+     * model.addAttribute(item) 자동 추가
+     */
     @PostMapping("/add")
     public String addItemV4(Item item) {
         itemRepository.save(item);

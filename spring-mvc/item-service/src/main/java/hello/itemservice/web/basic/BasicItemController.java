fc5dc562ef64de275628bf7b45444f8a815586b1
meeansub <welcltm@gmail.com>
2021-07-23 10:24:33 +0900

    상품 수정

--- spring-mvc/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring-mvc/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -26,7 +26,7 @@ public class BasicItemController {
     }
 
     @GetMapping("/{itemId}")
-    public String item(@PathVariable long itemId, Model model) {
+    public String item(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
 
@@ -88,6 +88,20 @@ public class BasicItemController {
     public String addItemV4(Item item) {
         itemRepository.save(item);
         return "basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
 

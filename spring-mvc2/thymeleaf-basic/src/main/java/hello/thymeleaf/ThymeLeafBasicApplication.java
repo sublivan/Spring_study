d0ca5eda501dc60438426e903a4ca66495bb3448
meeansub <welcltm@gmail.com>
2021-08-11 15:48:06 +0900

    Project init

---
+++ spring-mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/ThymeLeafBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafBasicApplication.class, args);
+	}
+
+}
